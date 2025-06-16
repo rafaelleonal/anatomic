d2ab647ac29506f884f6f905698eddeb42e80135
Rafael León Alfaro <rafaelleonal95@gmail.com>
2025-06-15 21:06:39 -0600

    deployment: fix lint errors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { MessageCircle } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardHeader,
